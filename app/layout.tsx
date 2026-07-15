09b1902c37cfd4da6df3d63ce868e008dbf40288
dattavw <ct.bopi@gmail.com>
2026-07-15 18:58:03 -0400

    initial hello version 1.0.0

---
+++ app/layout.tsx
@@ -0,0 +1,5 @@
+﻿export const metadata = { title: "Blue Green Hello" };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <html lang="es"><body style={{ margin: 0 }}>{children}</body></html>;
+}
